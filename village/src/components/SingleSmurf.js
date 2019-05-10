3d0e6d5ddde76f1dbbc722d5ed6dd20757e591b9
Seth Lachman <lachmanseth17@gmail.com>
2019-05-10 14:23:04 -0400

    Added basic styles to single smurf page.

--- village/src/components/SingleSmurf.js
+++ village/src/components/SingleSmurf.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Container = styled.div`margin: 3.5rem 0 0;`;
+
+const Title = styled.h2`
+	margin: 0 0 2rem;
+	font-size: 2.3rem;
+`;
+const Text = styled.p`font-size: 1.7rem;`;
 
 class SingleSmurf extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      smurf: null
-    }
-  }
+		this.state = {
+			smurf: null
+		};
+	}
 
-  componentDidMount() {
-    axios.get(`http://localhost:3333/getSmurfById/${this.props.match.params.id}`)
-    .then(res => this.setState({ smurf: res.data}))
-    .catch(err => console.log(err))
-  }
+	componentDidMount() {
+		axios
+			.get(`http://localhost:3333/getSmurfById/${this.props.match.params.id}`)
+			.then((res) => this.setState({ smurf: res.data }))
+			.catch((err) => console.log(err));
+	}
 
-  render() {
-    const { smurf } = this.state;
+	render() {
+		const { smurf } = this.state;
 
-    if (!smurf) {
-      return <h2>Loading Smurf Data...</h2>
-    }
-    return (
-      <div>
-        <h2>Name: {smurf.name}</h2>
-        <p>Height: {smurf.height}</p>
-        <p>Age: {smurf.age}</p>
-      </div>
-    )
-  }
-
+		if (!smurf) {
+			return <Title>Loading Smurf Data...</Title>;
+		}
+		return (
+			<Container>
+				<Title>Name: {smurf.name}</Title>
+				<Text>Height: {smurf.height}</Text>
+				<Text>Age: {smurf.age}</Text>
+			</Container>
+		);
+	}
 }
-
 
 export default SingleSmurf;
