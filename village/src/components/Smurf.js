39576567afc9150a9d0ff343aa313a3949d2296f
Seth Lachman <lachmanseth17@gmail.com>
2019-05-10 12:12:19 -0400

    Added styles to tiles on front page.

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const Tile = styled.div`
+  background: #0ea8eb;
+  padding: 2.5rem;
+  color: #f9f9f9;
+  margin: 1rem 1.5rem 0;
+`;
+
+const Title = styled.h3`
+  font-size: 1.8em;
+`;
+
+const Text = styled.p`
+  font-size: 1.5rem;
+`;
+
+const BoldText = styled(Text)`
+  font-weight: 700;
+`;
+
 const Smurf = props => {
   return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
+    <Tile>
+      <Title>{props.name}</Title>
+      <BoldText>{props.height} tall</BoldText>
+      <Text>{props.age} smurf years old</Text>
+    </Tile>
   );
 };
 
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import Smurf from './Smurf';
+
+const SmurfContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
 
 class Smurfs extends Component {
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
+          <SmurfContainer>
+            {this.props.smurfs.map(smurf => {
+              return (
+                <Smurf
+                  name={smurf.name}
+                  id={smurf.id}
+                  age={smurf.age}
+                  height={smurf.height}
+                  key={smurf.id}
+                />
+              );
+            })}
+          </SmurfContainer>
       </div>
     );
   }
