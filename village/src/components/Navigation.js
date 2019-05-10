f12102f01fa73d5b2c2ed828067a74903ecfe3ff
Seth Lachman <lachmanseth17@gmail.com>
2019-05-10 11:59:23 -0400

    Added styled components and styling to navigation bar.

--- village/src/components/Navigation.js
+++ village/src/components/Navigation.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  background: #E6E6FA;
+`;
+
+const NavBar = styled.nav`
+  padding: 2rem;
+`;
+
+const ListContainer = styled.ul`
+  margin: 0;
+  list-style: none;
+  display: flex;
+`;
+
+const ListItem = styled.li`
+  margin: 0 1.2rem;
+`;
+
+const NavElement = styled(NavLink)`
+  color: #0ea8eb;
+  font-size: 1.5rem;
+  text-decoration: none;
+  font-weight: bold;
+`;
+
 
 const Navigation = () => {
   return ( 
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/smurf-form">Add Smurf</NavLink></li>
-      </ul>
-    </nav>
+    <Header>
+      <NavBar>
+        <ListContainer>
+          <ListItem><NavElement to="/">Home</NavElement></ListItem>
+          <ListItem><NavElement to="/smurf-form">Add Smurf</NavElement></ListItem>
+        </ListContainer>
+      </NavBar>
+    </Header>
   )
 }
 
