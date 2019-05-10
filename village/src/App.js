519b316c4135398f942353efff4dd0a50cf8d2f8
Seth Lachman <lachmanseth17@gmail.com>
2019-05-10 11:37:53 -0400

    Added react router and routes for home and smurf-form.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -32,8 +33,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm postSmurf={this.postSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route path="/smurf-form" render={props => <SmurfForm {...props} postSmurf={this.postSmurf} />}/>
       </div>
     );
   }
